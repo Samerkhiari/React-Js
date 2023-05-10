9d7570ff46748f2c332e5087edfe43a4b2dd26a1
samerkhiari <127328803+Samerkhiari@users.noreply.github.com>
2023-05-10 17:30:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Form from "./Myform";
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    <Form/> 
+    </div>
+    </>
+  );
+}
+
+export default App;
